d14edfd5300d358043d3e7815e81f1a2fff26615
trangjackie <dldtrang@gmail.com>
2018-04-14 20:43:38 +0700

    ui

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,9 +6,108 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    // UART FPGA
+    uart_fpga_setting = new SettingsDialog;
+    uart_fpga_setting->setdefault(0,3);
+    uart_fpga = new QSerialPort(this);
+    connect(uart_fpga, SIGNAL(error(QSerialPort::SerialPortError)), this,
+            SLOT(uart_fpga_handleError(QSerialPort::SerialPortError)));
+    connect(uart_fpga, SIGNAL(readyRead()), this, SLOT(uart_fpga_readData()));
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_pushButton_app_close_clicked()
+{
+    QMessageBox msgBox;
+    msgBox.setText("Quit program.");
+    msgBox.setInformativeText("Do you want to leave?");
+    msgBox.setStandardButtons(QMessageBox::Yes | QMessageBox::Cancel);
+    msgBox.setDefaultButton(QMessageBox::Cancel);
+    int ret = msgBox.exec();
+    switch (ret) {
+      case QMessageBox::Yes:
+          // Yes was clicked
+        QApplication::quit();
+          break;
+      case QMessageBox::Cancel:
+          // Cancel was clicked
+          break;
+      default:
+          // should never be reached
+          break;
+    }
+}
+
+void MainWindow::on_pushButton_uart_fpga_setting_clicked()
+{
+    uart_fpga_setting->show();
+}
+
+void MainWindow::on_pushButton_uart_fpga_connect_clicked()
+{
+    if (ui->pushButton_uart_fpga_connect->text()=="Connect") // have not connected
+    {
+        SettingsDialog::Settings p = uart_fpga_setting->settings();
+        uart_fpga->setPortName(p.name);
+        uart_fpga->setBaudRate(p.baudRate);
+        uart_fpga->setDataBits(p.dataBits);
+        uart_fpga->setParity(p.parity);
+        uart_fpga->setStopBits(p.stopBits);
+        uart_fpga->setFlowControl(p.flowControl);
+        if (uart_fpga->open(QIODevice::ReadWrite)) {
+            ui->pushButton_uart_fpga_connect->setToolTip("Click to disconnect UART.");
+            ui->pushButton_uart_fpga_connect->setText("Disconnect");
+            ui->pushButton_uart_fpga_setting->setEnabled(false);
+            ui->statusBar->showMessage(QString("Connected to %1 : %2, %3, %4, %5, %6")
+                              .arg(p.name).arg(p.stringBaudRate).arg(p.stringDataBits)
+                              .arg(p.stringParity).arg(p.stringStopBits).arg(p.stringFlowControl));
+        } else {
+            QMessageBox::critical(this, tr("Error"), uart_fpga->errorString());
+            ui->statusBar->showMessage("Open error");
+        }
+    }
+    else // connected already
+    {
+        QMessageBox msgBox;
+        msgBox.setText("Disconnect serial port");
+        msgBox.setInformativeText("Are you sure?");
+        msgBox.setStandardButtons(QMessageBox::Yes | QMessageBox::Cancel);
+        msgBox.setDefaultButton(QMessageBox::Cancel);
+        int ret = msgBox.exec();
+        switch (ret) {
+          case QMessageBox::Yes:
+            // Yes was clicked--> disconnect
+            if (uart_fpga->isOpen())
+                uart_fpga->close();
+            //console->setEnabled(false);
+            ui->pushButton_uart_fpga_connect->setText("Connect");
+            ui->pushButton_uart_fpga_connect->setToolTip("Click to connect UART.");
+            ui->pushButton_uart_fpga_setting->setEnabled(true);
+            ui->statusBar->showMessage("UART fpga disconnected");
+              break;
+          case QMessageBox::Cancel:
+            // Cancel was clicked
+            break;
+          default:
+            // should never be reached
+            break;
+        }
+    }
+}
+
+void MainWindow::uart_fpga_readData()
+{
+
+}
+
+void MainWindow::uart_fpga_handleError(QSerialPort::SerialPortError error)
+{
+    if (error == QSerialPort::ResourceError) {
+    QMessageBox::critical(this, QString("Critical Error"), uart_fpga->errorString());
+    }
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QMessageBox>
 #include "uart/settingsdialog.h"
 
 
@@ -17,10 +18,21 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    void on_pushButton_app_close_clicked();
+
+    void on_pushButton_uart_fpga_setting_clicked();
+
+    void on_pushButton_uart_fpga_connect_clicked();
+
+    void uart_fpga_readData();
+
+    void uart_fpga_handleError(QSerialPort::SerialPortError error);
+
 private:
     Ui::MainWindow *ui;
     SettingsDialog *uart_fpga_setting;
-
+    QSerialPort *uart_fpga;
 };
 
 #endif // MAINWINDOW_H
